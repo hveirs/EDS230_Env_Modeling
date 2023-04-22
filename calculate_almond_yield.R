a0bbd626aababaaf7770befd964e5474099c699a
hveirs <hveirs3@outlook.com>
2023-04-22 15:49:40 -0700

    added comments for almond function

---
+++ calculate_almond_yield.R
@@ -0,0 +1,46 @@
+options(scipen = 999)
+
+#' Calculating Almond Crop Yield Statistics
+#' 
+#' Computes the maximum, minimum, and average almond yield when given a data frame containing monthly climate observations of: minimum and maximum temperature in °C, and precipitation in mm.
+#' @param climate_data 
+#'
+#' @return Minimum almond yield, mean almond yield, maximum almond yield
+#' 
+#'
+#' @examples calculate_almond_yield(climate_data = climate_data)
+#' @export 
+calculate_almond_yield <- function(climate_data) {
+  
+  # Calculate monthly summaries of climate variables
+  climate_summary <- climate_data %>%
+    group_by(month, year) %>% # Group by month and year
+    summarize(total_precip = sum(precip), # Then calculate the summaries
+              mean_tmax = mean(tmax_c),
+              mean_tmin = mean(tmin_c),
+              .groups = "drop") %>% # Drop the grouping levels of the output
+    arrange(year) # Order the rows based on the year column
+  
+  # Extract necessary columns
+  precip_month1 <- climate_summary$total_precip[which(climate_summary$month == 1)] # Precipitation for January
+  min_temp_month2 <- climate_summary$mean_tmin[which(climate_summary$month == 2)] # Minimum Temperature for Feb
+  # These code lines index and extract the total precipitation and temperature values for when the month was January and February (respectively).
+  
+  # Calculate yield using equation
+  yield <- (0.015 * min_temp_month2) - (0.0046 * min_temp_month2^2) - (0.07 * precip_month1) + (0.0043 * precip_month1^2) + 0.28
+  
+  # Save and return min, max, and average almond yield
+  min_yield = min(yield)
+  mean_yield = mean(yield)
+  max_yield = max(yield)
+  
+  # Print the returned values
+  print(paste("Minimum Almond Yield =", min_yield))
+  print(paste("Average Almond Yield =", mean_yield))
+  print(paste("Maximum Almond Yield =", max_yield))
+  
+}
+
+# Test the function!
+calculate_almond_yield(climate_data = climate_data)
+
